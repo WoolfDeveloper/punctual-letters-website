754f4375ad6bde0c58cb66895954aec14b9e10a7
WoolfDeveloper <lluclanser@gmail.com>
2024-06-01 07:48:00 +0200

    Add --trace to config in build-and-serve.sh

--- build-and-serve.sh
+++ build-and-serve.sh
@@ -4,4 +4,4 @@ SERVER_HOST="${SERVER_HOST:-127.0.0.1}"
 SERVER_PORT="${SERVER_PORT:-4000}"
 
 bundle install
-bundle exec jekyll serve --config _config.yml,_config.development.yml --host "$SERVER_HOST" --port "$SERVER_PORT"
+bundle exec jekyll serve --trace --config _config.yml,_config.development.yml --host "$SERVER_HOST" --port "$SERVER_PORT"
